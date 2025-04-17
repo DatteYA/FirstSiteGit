2f0c6a1b111945031edb7de2f6d593f1eb823560
DatteYA <lesha.alekhin.2018@mail.ru>
2025-04-17 12:23:22 +0300

    Create users table

--- core/models/users.py
+++ core/models/users.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
 
 
-#class User(Base):
-    #nickname: Mapped[int] = mapped_column(String(20), unique=True)
+class User(Base):
+    nickname: Mapped[int] = mapped_column(String(20), unique=True)
 
 
